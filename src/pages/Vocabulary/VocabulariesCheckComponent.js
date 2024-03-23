6dadab7410e563f77d089e295570dd2241a94aae
VuDoan <bavudoan@gmail.com>
2024-03-23 17:33:37 +0700

    integrator api - done

--- src/pages/Vocabulary/VocabulariesCheckComponent.js
+++ src/pages/Vocabulary/VocabulariesCheckComponent.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {Controller, useForm} from "react-hook-form";
 import {Image} from "react-bootstrap";
-import {fetchGet} from "../../common/fetchCommon";
+import {VocabulariesService} from "../../services/vocabulariesService";
 
 const VocabulariesCheckComponent = () => {
-    const vocabularies = ['medicine', 'refrigerator', 'emergency', 'lifeguard']
+    // const vocabularies = ['medicine', 'refrigerator', 'emergency', 'lifeguard']
+    const [vocabularies, setVocabularies] = useState([])
     const [vocabularyIndex, setVocabularyIndex] = useState(0)
     const [isStarted, setIsStarted] = useState(true)
     const [answer, setAnswer] = useState('')
@@ -21,82 +22,100 @@ const VocabulariesCheckComponent = () => {
             vocabulary: ''
         }
     });
-
+    const handleUpdateVocabulary = (id) => {
+        VocabulariesService.updateVocabularies(id).then();
+    }
     const handleSpeak = (vocabulary) => {
-        window.responsiveVoice.speak(vocabulary)
+        if (vocabulary) {
+            window.responsiveVoice.speak(vocabulary)
+        } else {
+            window.responsiveVoice.speak("Vocabulary has not found")
+        }
+
+
     }
     const onSubmit = () => {
         const {vocabulary} = getValues()
-        if (vocabulary === vocabularies[vocabularyIndex]) {
+        if (vocabulary === vocabularies[vocabularyIndex].vocabulary) {
             setAnswer('true')
             setVocabularyIndex(prevState => ++prevState);
             reset();
+            const vocabularyPassed = vocabularies.findLast(v => v.vocabulary = vocabulary);
+            if (vocabularyPassed && vocabularyPassed.id) {
+                handleUpdateVocabulary(vocabularyPassed.id);
+            }
         } else {
             setAnswer('false')
-            handleSpeak(vocabularies[vocabularyIndex])
+            handleSpeak(vocabularies[vocabularyIndex].vocabulary)
             setError('vocabulary', {message: 'Wrong vocabulary. Input again!'})
         }
     };
     const handleListenAgain = () => {
-        handleSpeak(vocabularies[vocabularyIndex])
+        handleSpeak(vocabularies[vocabularyIndex].vocabulary)
     }
 
     useEffect(() => {
-        if (isStarted) {
-            handleSpeak(vocabularies[vocabularyIndex])
+        if (isStarted && vocabularies[vocabularyIndex]) {
+            handleSpeak(vocabularies[vocabularyIndex].vocabulary)
         }
-        if (vocabularyIndex === vocabularies.length) {
-            alert('may hoc xong roi do')
+        if (vocabularyIndex === vocabularies.length && vocabularyIndex > 0) {
+            alert('You have been finish your course today')
+            handleSpeak('You have been finish your course today')
         }
     }, [vocabularyIndex, isStarted]);
 
     const getVocabularies = () => {
-        fetchGet('api/v1/vocabularies/list', null).then(res => {
-
+        VocabulariesService.getList().then(data => {
+            if (data && data.status === 0) {
+                setVocabularies(data.data)
+            } else {
+                alert('You have been finish your course today')
+                handleSpeak('You have been finish your course today')
+            }
         })
     }
     useEffect(() => {
-        // todo get all vocabularies
         getVocabularies();
     }, []);
 
     return (
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className={'d-flex justify-content-center gap-1 mt-4'}>
-                    <button type={"submit"} className={'btn btn-success'}>❮ Previous</button>
-                    <Controller
-                        control={control}
-                        name={'vocabulary'}
-                        rules={{
-                            required: {value: true, message: 'Nhap tu vao'},
-                        }}
-                        render={({field, formState, fieldState}) => (
-                            <input
-                                className={`form-control input-group-sm w-25 ${errors.vocabulary ? 'is-invalid' : ''}`}
-                                {...field}
-                            />)}
-                    />
-                    <button type={"submit"} className={'btn btn-success'}>Next ❯</button>
-                    <button type={"button"} className={'btn btn-success'} onClick={handleListenAgain}>Listen
-                        again!
-                    </button>
-                </div>
-                {errors.vocabulary &&
-                    <label className={'text-center w-100 text-danger mt-2'}>{errors.vocabulary.message}</label>}
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={'d-flex justify-content-center gap-1 mt-4'}>
+                <button type={"submit"} className={'btn btn-success'}>❮ Previous</button>
+                <Controller
+                    control={control}
+                    name={'vocabulary'}
+                    rules={{
+                        required: {value: true, message: 'Nhap tu vao'},
+                    }}
+                    render={({field, formState, fieldState}) => (
+                        <input
+                            className={`form-control input-group-sm w-25 ${errors.vocabulary ? 'is-invalid' : ''}`}
+                            {...field}
+                        />)}
+                />
+                <button type={"submit"} className={'btn btn-success'}>Next ❯</button>
+                <button type={"button"} className={'btn btn-success'} onClick={handleListenAgain}>Listen
+                    again!
+                </button>
+            </div>
+            {errors.vocabulary &&
+                <label className={'text-center w-100 text-danger mt-2'}>{errors.vocabulary.message}</label>}
+            {vocabularies[vocabularyIndex] && vocabularies[vocabularyIndex].imageDescription &&
                 <div className={'text-center mt-4'}>
-                    <Image src={'https://i.pinimg.com/originals/fe/01/3f/fe013f692231e4e61376f11c49779440.gif'}
+                    <Image src={vocabularies[vocabularyIndex].imageDescription}
                            style={{height: 100}}/>
-                </div>
-                <div className={'d-flex justify-content-center mt-4'}>
-                    {answer === 'false' && <Image
-                        src={'https://media.baamboozle.com/uploads/images/670774/6569f919-9802-473f-a7ff-282fae2d90f1.gif'}
+                </div>}
+            <div className={'d-flex justify-content-center mt-4'}>
+                {answer === 'false' && <Image
+                    src={'https://media.baamboozle.com/uploads/images/670774/6569f919-9802-473f-a7ff-282fae2d90f1.gif'}
+                    style={{height: 100}}/>}
+                {answer === 'true' &&
+                    <Image
+                        src={'https://i.pinimg.com/originals/fe/01/3f/fe013f692231e4e61376f11c49779440.gif'}
                         style={{height: 100}}/>}
-                    {answer === 'true' &&
-                        <Image
-                            src={'https://i.pinimg.com/originals/fe/01/3f/fe013f692231e4e61376f11c49779440.gif'}
-                            style={{height: 100}}/>}
-                </div>
-            </form>
+            </div>
+        </form>
     );
 };
 
