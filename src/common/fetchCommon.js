0a8c0674088b93416703659fd82b9e56c72fdf95
Anthony.Doan <anthony.doan@code88.asia>
2024-06-18 17:24:17 +0700

    update

--- src/common/fetchCommon.js
+++ src/common/fetchCommon.js
@@ -1,36 +1,17 @@
 import {HttpStatus} from './HttpStatus';
 
-export const fetchGet = async (url, data) => {
-    if (data) {
-        url = url + '?' + new URLSearchParams(data).toString();
-    }
-    let headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Origin': '*',
-    };
-    if (localStorage.getItem('access_token')) {
-        headers.Authorization = 'Bearer ' + localStorage.getItem('access_token');
-    }
-    return fetch(process.env.REACT_APP_WEB_SERVICE_URL + url, {
-        method: 'GET',
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: headers,
-    }).then(res => {
-        if (res.status === HttpStatus.UNAUTHORIZED) {
-            window.location.reload();
-            localStorage.removeItem('user');
-        } else if (res.status === HttpStatus.SUCCESS) {
-            return Promise.resolve(res.json());
-        } else {
-            throw new Error('Something went wrong with status code ' + res.status);
-        }
-    }).catch(err => (err));
+export const fetchGet = (url, param) => {
+    return fetchCommon(url, null, 'GET', param);
 };
 
-export const fetchPost = async (url, data) => {
-    // const navigate = useNavigate();
+export const fetchPost = async (url, data, param) => {
+    return fetchCommon(url, data, 'POST', param);
+};
+
+export const fetchCommon = (url, data, method, param) => {
+    if (param) {
+        url = url + '?' + new URLSearchParams(param).toString();
+    }
     let headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
@@ -40,19 +21,40 @@ export const fetchPost = async (url, data) => {
         headers.Authorization = 'Bearer ' + localStorage.getItem('access_token');
     }
     return fetch(process.env.REACT_APP_WEB_SERVICE_URL + url, {
-        method: 'POST',
+        method: method,
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        body: JSON.stringify(data),
+        body: data ? JSON.stringify(data) : null,
         headers: headers,
     }).then(res => {
-        if (res.status === HttpStatus.UNAUTHORIZED) {
-            window.location.reload();
-            localStorage.removeItem('user');
-        } else if (res.status === HttpStatus.SUCCESS) {
-            return Promise.resolve(res.json());
+        if (!res.ok) {
+            if (res.status === HttpStatus.UNAUTHORIZED) {
+                localStorage.removeItem('user');
+                localStorage.removeItem('access_token');
+                window.location.reload();
+            }
+            throw new Error(JSON.stringify({status: res.status, message: res.statusText}));
         } else {
-            throw new Error('Something went wrong with status code ' + res.status);
+            return res;
         }
-    }).catch(err => (err));
+    }).then((res) => res.json())
+        .then(data => {
+            if (!data) {
+                throw new Error('No body found in response of url ' + url);
+            }
+            if (data.status === HttpStatus.UNAUTHORIZED) {
+                localStorage.removeItem('user');
+                localStorage.removeItem('access_token');
+                window.location.reload();
+                throw new Error('Authenticate fail:  ' + data.message);
+            } else if (data.status === HttpStatus.SUCCESS) {
+                return Promise.resolve(data);
+            } else {
+                throw new Error(JSON.stringify(data));
+            }
+        }).catch(err => {
+            const error = JSON.parse(err.message);
+            console.log(error.message);
+            return Promise.resolve(error);
+        });
 };
--- src/services/vocabulariesService.js
+++ src/services/vocabulariesService.js
@@ -2,12 +2,12 @@ import {fetchGet, fetchPost} from '../common/fetchCommon';
 
 export const VocabulariesService = {
     getList: () => fetchGet('/vocabulary/list', null),
-    addVocabularies: (vocabularies) => fetchPost('/vocabulary/add-all', vocabularies),
-    addVocabulary: (vocabularies) => fetchPost('/vocabulary/add', vocabularies),
+    addVocabularies: (vocabularies) => fetchPost('/vocabulary/add-all', vocabularies, null),
+    addVocabulary: (vocabularies) => fetchPost('/vocabulary/add', vocabularies, null),
     updateVocabularies: (data) => fetchGet('/vocabulary/update', data),
     getReport: () => fetchGet('/vocabulary/report', null),
     updateReportToday: (lesson) => fetchGet('/vocabulary/report/lesson/' + lesson, null),
     getReportToday: () => fetchGet('/vocabulary/report/today', null),
-    login: (data) => fetchPost('/auth/login', data),
-    logout: (data) => fetchPost('/auth/logout', data)
+    login: (data) => fetchPost('/auth/login', data, null),
+    logout: (data) => fetchPost('/auth/logout', data, null)
 };
