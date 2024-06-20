fed6461a4b5f7c3701687f523bc2a088bd74842f
Anthony.Doan <anthony.doan@code88.asia>
2024-06-20 17:29:21 +0700

    handle spinner

--- src/common/fetchCommon.js
+++ src/common/fetchCommon.js
@@ -3,16 +3,19 @@ import store from '../stores/ReduxStore';
 import {LoadingSlice} from '../stores/slices/LoadingSlice';
 import {LOGIN_PATH} from './roles';
 
-export const fetchGet = (url, param) => {
-    return fetchCommon(url, null, 'GET', param);
+export const fetchGet = (url, param, isShowSpinner) => {
+    return fetchCommon(url, null, 'GET', param, isShowSpinner);
 };
 
-export const fetchPost = async (url, data, param) => {
-    return fetchCommon(url, data, 'POST', param);
+export const fetchPost = async (url, data, param, isShowSpinner) => {
+    return fetchCommon(url, data, 'POST', param, isShowSpinner);
 };
 
 const {actions: LoadingActions} = LoadingSlice;
-export const fetchCommon = (url, data, method, param) => {
+export const fetchCommon = (url, data, method, param, isShowSpinner) => {
+    if (isShowSpinner) {
+        store.dispatch(LoadingActions.setIsLoading(true));
+    }
     if (param) {
         url = url + '?' + new URLSearchParams(param).toString();
     }
@@ -24,7 +27,7 @@ export const fetchCommon = (url, data, method, param) => {
     if (localStorage.getItem('access_token')) {
         headers.Authorization = 'Bearer ' + localStorage.getItem('access_token');
     }
-    store.dispatch(LoadingActions.setIsLoading(true));
+
     return fetch(process.env.REACT_APP_WEB_SERVICE_URL + url, {
         method: method,
         mode: 'cors', // no-cors, *cors, same-origin
--- src/services/vocabulariesService.js
+++ src/services/vocabulariesService.js
@@ -1,13 +1,13 @@
 import {fetchGet, fetchPost} from '../common/fetchCommon';
 
 export const VocabulariesService = {
-    getList: () => fetchGet('/vocabulary/list', null),
-    addVocabularies: (vocabularies) => fetchPost('/vocabulary/add-all', vocabularies, null),
-    addVocabulary: (vocabularies) => fetchPost('/vocabulary/add', vocabularies, null),
-    updateVocabularies: (data) => fetchGet('/vocabulary/update', data),
-    getReport: () => fetchGet('/vocabulary/report', null),
-    updateReportToday: (lesson) => fetchGet('/vocabulary/report/lesson/' + lesson, null),
-    getReportToday: () => fetchGet('/vocabulary/report/today', null),
-    login: (data) => fetchPost('/auth/login', data, null),
-    logout: (data) => fetchPost('/auth/logout', data, null)
+    getList: () => fetchGet('/vocabulary/list', null, true),
+    addVocabularies: (vocabularies) => fetchPost('/vocabulary/add-all', vocabularies, null, true),
+    addVocabulary: (vocabularies) => fetchPost('/vocabulary/add', vocabularies, null, true),
+    updateVocabularies: (data) => fetchGet('/vocabulary/update', data, false),
+    getReport: () => fetchGet('/vocabulary/report', null, true),
+    updateReportToday: (lesson) => fetchGet('/vocabulary/report/lesson/' + lesson, null, true),
+    getReportToday: () => fetchGet('/vocabulary/report/today', null, true),
+    login: (data) => fetchPost('/auth/login', data, null, true),
+    logout: (data) => fetchPost('/auth/logout', data, null, true)
 };
