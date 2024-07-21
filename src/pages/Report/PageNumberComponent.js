d667ea53179d54e9f969f402fe46d9ddc1f9ab5f
VuDoan <bavudoan@gmail.com>
2024-07-21 14:08:20 +0700

    update page options

--- src/pages/Report/PageNumberComponent.js
+++ src/pages/Report/PageNumberComponent.js
@@ -8,33 +8,11 @@ const PageNumberComponent = ({pageIndex, tableInstance}) => {
     const listNumberDisplay = useMemo(() => {
         // Display full number if number of page less or equal to 7
         // Ex : 1,2,3,4,5,6,7
-        if (pageOptions.length <= 6) {
-            return pageOptions;
-        } else if (pageOptions.length > 6) {
-            let arr = [0, '...', pageOptions.length - 1];
-            let arrayForm;
-            let insertAt = 2;
-
-            if (pageIndex <= 3) {
-                // Current page is less than 5
-                // Ex : 1,2,3,4,5 ... n
-                insertAt = 1;
-                arrayForm = Array.from({length: 4}, (_, idx) => idx + 1);
-            } else if (pageIndex >= pageOptions.length - 4) {
-                // Current page is greater than or equal to n - 4
-                // Ex : 1...6,7,8,9,10
-                arrayForm = Array.from({length: 4}, (_, idx) => pageOptions.length - 5 + idx);
-            } else {
-                // Current page is between 5 and n - 4
-                // Ex : 1... 6,7,8 ...,n
-                arrayForm = [pageIndex - 1, pageIndex, pageIndex + 1, '...'];
-            }
-
-            return arr.insertAt(insertAt, arrayForm);
-        }
+        return pageOptions;
     }, [pageOptions, pageIndex]);
 
     const goToPage = (index) => () => setPageIndex(index);
+
     return (
         <div className='dataTables_paginate paging_full_numbers'>
             <a className='paginate_button' onClick={goToPage(0)} hidden={!getCanPreviousPage()}>
