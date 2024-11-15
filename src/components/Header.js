d4ae7304ac7fcbc3397d5b61e5d4ecdb0ec1b3c0
VuDoan <bavudoan@gmail.com>
2024-11-15 07:27:43 +0700

    update report

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,12 @@ import {Dropdown, Image} from 'react-bootstrap';
 import guest from '../assets/guest.jpeg';
 import {VocabulariesService} from '../services/vocabulariesService';
 import {LOGIN_PATH, PROFILE_PATH} from '../common/roles';
+import {ACCESS_TOKEN, USER_KEY} from "../common/constants";
+import fire from "../assets/fire.png";
 
 export const Header = () => {
     const navigate = useNavigate();
-    const userString = localStorage.getItem('user');
+    const userString = localStorage.getItem(USER_KEY);
     const userInfo = useMemo(() => {
         if (!!userString) {
             return JSON.parse(userString);
@@ -18,17 +20,15 @@ export const Header = () => {
         }
     }, [userString]);
 
-    // const userInfo = {avatar: guest, username: ''};
-
     const logout = () => {
-        const token = localStorage.getItem('access_token');
+        const token = localStorage.getItem(ACCESS_TOKEN);
         const logoutRequest = {
             username: userInfo.username,
             token: token
         };
         VocabulariesService.logout(logoutRequest).then();
-        localStorage.removeItem('user');
-        localStorage.removeItem('access_token');
+        localStorage.removeItem(USER_KEY);
+        localStorage.removeItem(ACCESS_TOKEN);
         navigate(LOGIN_PATH);
     };
 
@@ -44,10 +44,14 @@ export const Header = () => {
 
     return (
         <header className="site-header">
-            <div className="site-identity">
+            <div className="site-identity d-flex flex-row">
                 <h1>
                     <Link to={process.env.PUBLIC_URL + '/'}>Vocabularies Page</Link>
                 </h1>
+                <div className={'d-flex align-items-center ps-2'}>
+                    <Image src={fire} style={{width: 20, height: 20}}/>
+                    <label className={'ps-1'}>{userInfo.stayLearningTimes || 0}</label>
+                </div>
             </div>
             <nav className="site-navigation">
                 <ul className="nav">
